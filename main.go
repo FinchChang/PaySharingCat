6bbbd92723fcd54f50ae19f515776944f611f7f3
Finch <garybo14@gmail.com>
2020-06-04 16:41:49 +0800

    <test>: fix build error

--- main.go
+++ main.go
@@ -143,9 +143,9 @@ func getReplyMsg(message, userID string) string {
 	var replyMsg string
 	if i > -1 {
 		//replyMsg = getActionMsg(string(MegRune[i+1:]), userID)
-		replyMsg := "---功能回覆---\n" + getActionMsg(string(MegRune[i+1:]), userID)
+		replyMsg = "---功能回覆---\n" + getActionMsg(string(MegRune[i+1:]), userID)
 		replyMsg += "\n---使用者訊息---\n" + message.Text
-		replyMsg += "\n---UserPorilfe---\n" + getUserProfile(event.Source.UserID)
+		replyMsg += "\n---UserPorilfe---\n" + getUserProfile(userID)
 	} else {
 		replyMsg = ""
 	}
