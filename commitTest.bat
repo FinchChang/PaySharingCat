87ea3354c643231aa2887314fc868f6f4f56658b
Finch <garybo14@gmail.com>
2020-06-18 23:03:45 +0800

    commit test code

---
+++ commitTest.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -m "commit test code"
+git push
+pause
--- main.go
+++ main.go
@@ -283,9 +283,8 @@ func testInsert(source *linebot.EventSource) string {
 	var sum string
 	// Send the query to the server. The returned rows MUST be closed
 	// before conn can be used again.
-	log.Println("time.now=", time.Now())
-	rows, err := conn.Query(context.Background(), `INSERT INTO public."GroupProfile" ("GroupID", "UserID", "UserName", "Num", "Time") VALUES($1,$2,$3,$4,$5)`, `'`+source.GroupID+`'`, `'`+source.UserID+`'`, `'`+getUserName(source.UserID)+`'`, getGroupCount(conn, source), `
-	2020/06/17`)
+	log.Println("GroupID=", source.GroupID, "UserID=", source.UserID, "UserName=", getUserName(source.UserID), "GroupCount=", getGroupCount(conn, source), "time=", time.Now())
+	rows, err := conn.Query(context.Background(), `INSERT INTO public."GroupProfile" ("GroupID", "UserID", "UserName", "Num", "Time") VALUES($1,$2,$3,$4,$5)`, source.GroupID, source.UserID+`'`, getUserName(source.UserID), getGroupCount(conn, source), time.Now())
 	if err != nil {
 		return err.Error()
 	}
