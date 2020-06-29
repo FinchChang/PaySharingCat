03a521faf2c305de12af81191c4f3629197d341b
Finch <garybo14@gmail.com>
2020-06-29 10:44:22 +0800

    test select

--- main.go
+++ main.go
@@ -271,8 +271,8 @@ func getGroupCount(source *linebot.EventSource) string {
 	defer db.Close()
 
 	var num int
-	//row := db.QueryRow(`SELECT COUNT("GID") FROM "GroupProfile" WHERE "GroupID"=$1`, `Cbe139f327d382569c3b709847caf4cc1`)
-	row := db.QueryRow(`SELECT COUNT("GID") FROM "GroupProfile" WHERE "GroupID" = $1`, source.GroupID)
+	row := db.QueryRow(`SELECT COUNT("GID") FROM "GroupProfile" WHERE "GroupID"=$1`, `Cbe139f327d382569c3b709847caf4cc1`)
+	//row := db.QueryRow(`SELECT COUNT("GID") FROM "GroupProfile" WHERE "GroupID" = $1`, source.GroupID)
 	err = row.Scan(&num)
 	if err != nil {
 		log.Fatal("get row data error: ", err)
